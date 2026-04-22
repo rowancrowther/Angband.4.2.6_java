a11be43cab53566f31c104e1142d5bb3645825fb
rowan <rowan@rowancrowther.me.uk>
2026-04-22 11:45:09 +0100

    GlyphType enum implemented, commented and tested
    Signed-off-by: rowan <rowan@rowancrowther.me.uk>

---
+++ src/main/java/uk/co/jackoftrades/middle/enums/GlyphType.java
@@ -0,0 +1,21 @@
+package uk.co.jackoftrades.middle.enums;
+
+/**
+ * The possible types of Glyph in the game
+ */
+public enum GlyphType {
+    /**
+     * No glyph
+     */
+    GLYPH_NONE,
+
+    /**
+     * A warding glyph
+     */
+    GLYPH_WARDING,
+
+    /**
+     * A decoy glyph
+     */
+    GLYPH_DECOY
+}

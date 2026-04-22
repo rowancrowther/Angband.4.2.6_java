e68148ae99e82705793f76f23078c3e2e49050db
rowan <rowan@rowancrowther.me.uk>
2026-04-22 13:09:07 +0100

    EffectInfo enum Implemented and Tested. NOT Commented, marked as TODO
    Signed-off-by: rowan <rowan@rowancrowther.me.uk>

---
+++ src/main/java/uk/co/jackoftrades/middle/enums/EffectInfo.java
@@ -0,0 +1,27 @@
+package uk.co.jackoftrades.middle.enums;
+
+/**
+ * TODO: Comment this
+ */
+public enum EffectInfo {
+    EFINFO_NONE,
+    EFINFO_DICE,
+    EFINFO_HEAL,
+    EFINFO_CONST,
+    EFINFO_FOOD,
+    EFINFO_CURE,
+    EFINFO_TIMED,
+    EFINFO_STAT,
+    EFINFO_SEEN,
+    EFINFO_SUMM,
+    EFINFO_TELE,
+    EFINFO_QUAKE,
+    EFINFO_BALL,
+    EFINFO_SPOT,
+    EFINFO_BREATH,
+    EFINFO_SHORT,
+    EFINFO_LASH,
+    EFINFO_BOLT,
+    EFINFO_BOLTD,
+    EFINFO_TOUCH
+}

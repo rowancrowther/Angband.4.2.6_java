da2f9f71fe8b4cd19fcbc3a234e08a3ca9e7c239
rowan <rowan@rowancrowther.me.uk>
2026-05-21 15:08:04 +0100

    Checked comments, overrides & set contracts on all Quit & QuitAux's methods
    Signed-off-by: rowan <rowan@rowancrowther.me.uk>

--- src/main/java/uk/co/jackoftrades/backend/utils/quit/Quit.java
+++ src/main/java/uk/co/jackoftrades/backend/utils/quit/Quit.java
@@ -1,16 +1,35 @@
+/*
+ * Copyright (c) 1987-2022 Angband contributors.
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the Angband licence:
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ *
+ *    Java code copyright (c) Rowan Crowther 2026
+ */
+
 package uk.co.jackoftrades.backend.utils.quit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 public class Quit implements QuitAux {
     private final Logger logger = LogManager.getLogger();
 
     /**
-     * @param quitMessage
+     * Standard quit - writes a message to the logger.info file and stops execution
+     * @param quitMessage The message to write to the log file
      */
     @Override
-    public void quit(String quitMessage) {
+    public void quit(@NotNull String quitMessage) {
         logger.info(quitMessage);
         // TODO: Remove comment on next line once this is totally tested
         // System.exit(0);
--- src/main/java/uk/co/jackoftrades/backend/utils/quit/QuitAux.java
+++ src/main/java/uk/co/jackoftrades/backend/utils/quit/QuitAux.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright (c) 1987-2022 Angband contributors.
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the Angband licence:
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ *
+ *    Java code copyright (c) Rowan Crowther 2026
+ */
+
 package uk.co.jackoftrades.backend.utils.quit;
 
 public interface QuitAux {
+
+    /**
+     * Quits the system with a specific message
+     *
+     * @param quitMessage The message to pass from the program as it quits
+     */
     void quit(String quitMessage);
 }
